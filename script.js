d81ef054e47c70a8d2beb5fccbfb602291bc1e10
warzyw12 <132128376+warzyw12@users.noreply.github.com>
2023-04-29 10:15:13 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,24 +1,21 @@
-const apiUrl = "https://api.openai.com/v1/engines/davinci-codex/completions";
-
-const promptText = `Please summarize the following content:
-{content}`;
-
-async function generateSummary(content) {
-  const response = await fetch(apiUrl, {
+async function generateSummary(text) {
+  const apiKey = "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";
+  const prompt = `Please summarize the following text:\n\n${text}`;
+  const response = await fetch("https://api.openai.com/v1/engines/davinci-codex/completions", {
     method: "POST",
     headers: {
       "Content-Type": "application/json",
-      "Authorization": `Bearer ${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}`,
+      "Authorization": `Bearer ${apiKey}`,
     },
     body: JSON.stringify({
-      prompt: promptText.replace("{content}", content),
-      max_tokens: 50,
+      prompt,
+      max_tokens: 100,
       n: 1,
       stop: null,
       temperature: 0.5,
     }),
   });
 
-  const result = await response.json();
-  return result.choices[0].text.trim();
+  const data = await response.json();
+  return data.choices[0].text;
 }
